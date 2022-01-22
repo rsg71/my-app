c7e726537914ee7d282ee11e0d505057c5a560f7
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-01-22 14:53:18 -0500

    create pages for home, portfolio, resume, and contact and install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/portfolio" element={<Portfolio/>} />
+          <Route path="/resume" element={<Resume/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Resume() {
+    return (
+        <div>
+            Resume Page
+        </div>
+    )
+        ;
+}
