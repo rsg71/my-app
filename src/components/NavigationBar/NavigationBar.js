89fbeb176956ac5cccf5743bf152839141f185b0
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-01-25 21:09:15 -0500

    replace favicon and modify navigation bar when user is on current page

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 // import img1 from '../../images/logo_white_dot.png';
 
 
 export default function NavigationBar() {
+    const { pathname } = useLocation();
+
 
     return (
         < >
@@ -22,10 +24,18 @@ export default function NavigationBar() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
-                            <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
-                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                            <Nav.Link as={Link} to="/" className={pathname === "/" && "text-white"}>
+                                Home
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/portfolio" className={pathname === "/portfolio" && "text-white"}>
+                                Portfolio
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/resume" className={pathname === "/resume" && "text-white"}>
+                                Resume
+                            </Nav.Link>
+                            <Nav.Link as={Link} to="/contact" className={pathname === "/contact" && "text-white"}>
+                                Contact
+                            </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
