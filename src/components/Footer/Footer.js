f9a42fa9fe425848c4eecbe693bcc17baa543384
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-01-25 21:23:34 -0500

    fix links in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
@@ -7,15 +8,19 @@ export default function Footer() {
                 <div className="row">
                     <div className="col">
                         <h5>Account Links</h5>
-                        <p><a href="https://github.com/rsg71" target="_blank" rel="noreferrer">Github <i className="fab fa-github"></i></a></p>
-                        <p><a href="https://www.linkedin.com/in/robert-greenawalt/" target="_blank" rel="noreferrer">LinkedIn <i
-                            className="fab fa-linkedin"></i></a></p>
+                        <p>
+                            <a href="https://github.com/rsg71" target="_blank" rel="noreferrer">Github <i className="fab fa-github"></i></a>
+                        </p>
+                        <p>
+                            <a href="https://www.linkedin.com/in/robert-greenawalt/" target="_blank" rel="noreferrer">LinkedIn <i
+                                className="fab fa-linkedin"></i></a>
+                        </p>
                     </div>
                     <div className="col">
                         <h5>Site Navigation</h5>
-                        <p><a href="/portfolio">Portfolio</a></p>
-                        <p><a href="/resume">Resume</a></p>
-                        <p><a href="/contact">Contact</a></p>
+                        <p><Link to="/portfolio">Portfolio</Link></p>
+                        <p><Link to="/resume">Resume</Link></p>
+                        <p><Link to="/contact">Contact</Link></p>
                     </div>
                     <div className="col">
                         <h5>Get in Touch</h5>
