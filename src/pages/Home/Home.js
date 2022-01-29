01529d0ed7735e30b348b21d4152da55f6a1092c
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-01-28 20:29:42 -0500

    edit layout of home and portfolio

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,86 +11,89 @@ export default function Home() {
         <div className="bg-black">
             <NavigationBar />
 
+            {/* md={{ span: 3, offset: 1 }} */}
+            <div>
+                <Row className="bg-black text-white my-5" >
+                    <Col xs={{ span: 11, offset: 1 }} md={{ span: 3, offset: 2 }} className="mb-4">
+                        <h1 style={{ fontSize: "3.5em", letterSpacing: ".05em" }} id="introHello">Hello,</h1>
+                    </Col>
+                    <Col xs={{ span: 11, offset: 1 }} md={{ span: 4 }} >
+                        <div className="homePageLinks">
+                            <h1>
+                                &#91; <a href="https://github.com/rsg71" className="text-decoration-none" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a> , <a href="https://www.linkedin.com/in/robert-greenawalt/" className="text-decoration-none" target="_blank" rel="noreferrer"><i
+                                    className="fab fa-linkedin"></i></a> , <a href="mailto:robert.greenawalt29@gmail.com" className="text-decoration-none"><i
+                                        className="far fa-envelope"></i></a> &#93;
+                            </h1>
+                        </div>
+                    </Col>
+                </Row>
 
-            <Container fluid className="px-0 appContainer">
 
-
-                <div className="homeContainer">
-                    <Row className="bg-black text-white paddingHomepage my-5" >
-                        <Col xs={12} md={6} className="mb-4">
-                            <h1 style={{ fontSize: "3.5em", letterSpacing: ".05em" }} id="introHello">Hello,</h1>
-                        </Col>
-                        <Col xs={12} md={6} >
-                            <div className="homePageLinks">
-                                <h1>
-                                    &#91; <a href="https://github.com/rsg71" className="text-decoration-none" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a> , <a href="https://www.linkedin.com/in/robert-greenawalt/" className="text-decoration-none" target="_blank" rel="noreferrer"><i
-                                        className="fab fa-linkedin"></i></a> , <a href="mailto:robert.greenawalt29@gmail.com" className="text-decoration-none"><i
-                                            className="far fa-envelope"></i></a> &#93;
-                                </h1>
+                <Row className="bg-black text-white my-5">
+                    <Col xs={{ span: 11, offset: 1 }} md={{ span: 8, offset: 2 }}>
+                        <div className="row animate__animated animate__fadeIn">
+                            <div>
+                                <h2 className="recursiveFont"> I'm Robert, a <em className="recursiveFont">full-stack developer</em> and lifelong
+                                    learner.</h2>
+                                <br />
+                                <p className="coreCompetencies">Core competencies include: MongoDB, MySQL, Express, React, and Node. Check
+                                    out
+                                    some of my work in my <a href="portfolio.html">portfolio</a>, add me on <a
+                                        href="https://www.linkedin.com/in/robert-greenawalt-43457a14a/" target="_blank" rel="noreferrer">LinkedIn</a>, or send me an
+                                    <a href="contact.html"> email</a>.
+                                </p>
                             </div>
-                        </Col>
-                    </Row>
+                        </div>
+                    </Col>
+                </Row>
 
+                <Row>
+                    <Col xs={{ span: 11, offset: 1 }} md={{ span: 8, offset: 2 }}>
 
-                    <Row className="bg-black text-white paddingHomepage my-5">
-                        <Col>
-                            <div className="row animate__animated animate__fadeIn">
-                                <div>
-                                    <h2 className="recursiveFont"> I'm Robert, a <em className="recursiveFont">full-stack developer</em> and lifelong
-                                        learner.</h2>
-                                    <br />
-                                    <p className="coreCompetencies">Core competencies include: MongoDB, MySQL, Express, React, and Node. Check
-                                        out
-                                        some of my work in my <a href="portfolio.html">portfolio</a>, add me on <a
-                                            href="https://www.linkedin.com/in/robert-greenawalt-43457a14a/" target="_blank" rel="noreferrer">LinkedIn</a>, or send me an
-                                        <a href="contact.html"> email</a>.
-                                    </p>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-
-                    <Row className="paddingHomepage mb-3">
-                        <Col sm={12}>
-                            <h3 className="projects animate__animated animate__fadeIn text-white">P r o j e c t s :</h3>
-                        </Col>
-                    </Row>
-
-                    <Row className="paddingHomepage mb-3">
-                        {myProjects.slice(0, 2).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
-                                <ProjectCard project={project} />
+                        <Row className="mb-3">
+                            <Col sm={12}>
+                                <h3 className="projects animate__animated animate__fadeIn text-white">P r o j e c t s :</h3>
                             </Col>
-                        ))}
-                    </Row>
+                        </Row>
 
-                    <Row className="paddingHomepage mb-3">
-                        {myProjects.slice(2, 4).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
+                        <Row className="mb-3">
+                            {myProjects.slice(0, 2).map(project => (
+                                <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
+                                    <ProjectCard project={project} />
+                                </Col>
+                            ))}
+                        </Row>
 
-                    <Row className="paddingHomepage mb-3">
-                        {myProjects.slice(4, 6).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
+                        <Row className="mb-3">
+                            {myProjects.slice(2, 4).map(project => (
+                                <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
+                                    <ProjectCard project={project} />
+                                </Col>
+                            ))}
+                        </Row>
 
-                    <Row className="paddingHomepage mb-3">
-                        {myProjects.slice(6, 8).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
-                </div>
+                        <Row className="mb-3">
+                            {myProjects.slice(4, 6).map(project => (
+                                <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
+                                    <ProjectCard project={project} />
+                                </Col>
+                            ))}
+                        </Row>
+
+                        <Row className="mb-3">
+                            {myProjects.slice(6, 8).map(project => (
+                                <Col key={project.id} sm={6} md={6} lg={6} className="mb-3">
+                                    <ProjectCard project={project} />
+                                </Col>
+                            ))}
+                        </Row>
+
+                    </Col>
+                </Row>
+
+            </div>
 
 
-            </Container>
-        </div>
+        </div >
     );
 }
--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -13,45 +13,53 @@ export default function Portfolio() {
         <div className="bg-black text-white">
 
             <NavigationBar />
-
+`
             <Jumbotron title="Portfolio" />
 
-            <Container fluid className="px-0 appContainer">
-                <div className="homeContainer">
+            <Row>
 
-                    <Row className="mt-3">
-                        {myProjects.slice(0, 2).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
+                
+                <Col sm={12} md={{ span: 9, offset: 3 }}>
+                    <Container fluid >
+                        <div >
 
-                    <Row>
-                        {myProjects.slice(2, 4).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
+                            <Row className="mt-3">
+                                {myProjects.slice(0, 2).map(project => (
+                                    <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
+                                        <ProjectCard project={project} />
+                                    </Col>
+                                ))}
+                            </Row>
 
-                    <Row>
-                        {myProjects.slice(4, 6).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
+                            <Row>
+                                {myProjects.slice(2, 4).map(project => (
+                                    <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
+                                        <ProjectCard project={project} />
+                                    </Col>
+                                ))}
+                            </Row>
 
-                    <Row>
-                        {myProjects.slice(6, 8).map(project => (
-                            <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
-                                <ProjectCard project={project} />
-                            </Col>
-                        ))}
-                    </Row>
-                </div>
-            </Container>
+                            <Row>
+                                {myProjects.slice(4, 6).map(project => (
+                                    <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
+                                        <ProjectCard project={project} />
+                                    </Col>
+                                ))}
+                            </Row>
+
+                            <Row>
+                                {myProjects.slice(6, 8).map(project => (
+                                    <Col key={project.id} sm={6} md={6} lg={4} className="mb-3">
+                                        <ProjectCard project={project} />
+                                    </Col>
+                                ))}
+                            </Row>
+                        </div>
+                    </Container>
+                </Col>
+
+            </Row>
+
 
         </div>
     );
