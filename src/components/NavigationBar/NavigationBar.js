f27fd35d7fe8b38f2191ea2f24ce209a30813f1f
Robert Greenawalt <robert.greenawalt29@gmail.com>
2022-07-12 23:27:32 -0400

    close navigation hamburger for mobile on link click

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 // import img1 from '../../images/logo_white_dot.png';
 
 
 export default function NavigationBar() {
+
+    const [expanded, setExpanded] = useState(false);
+
+    const closeMenu = () => setExpanded(false);
+
+    const toggleMenu = () => setExpanded(!expanded);
+
     const { pathname } = useLocation();
 
     const selectedPageClass = "text-white underline-red font-smaller";
@@ -12,9 +19,9 @@ export default function NavigationBar() {
 
     return (
         < >
-            <Navbar bg="black" expand="lg" variant="dark" className="recursiveFont">
+            <Navbar bg="black" expand="lg" expanded={expanded} variant="dark" className="recursiveFont">
                 <Container>
-                    <Navbar.Brand as={Link} to="/" >
+                    <Navbar.Brand as={Link} to="/" onClick={closeMenu}>
                         <img
                             src={"imgs/logo_white_dot.png"}
                             width="30"
@@ -23,19 +30,19 @@ export default function NavigationBar() {
                             alt="RG logo"
                         />
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={toggleMenu}/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/" className={pathname === "/" ? selectedPageClass : notSelected}>
+                            <Nav.Link as={Link} to="/" onClick={closeMenu} className={pathname === "/" ? selectedPageClass : notSelected}>
                                 Home
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/portfolio" className={pathname === "/portfolio" ? selectedPageClass : notSelected}>
+                            <Nav.Link as={Link} to="/portfolio" onClick={closeMenu} className={pathname === "/portfolio" ? selectedPageClass : notSelected}>
                                 Portfolio
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/resume" className={pathname === "/resume" ? selectedPageClass : notSelected}>
+                            <Nav.Link as={Link} to="/resume" onClick={closeMenu} className={pathname === "/resume" ? selectedPageClass : notSelected}>
                                 Resume
                             </Nav.Link>
-                            <Nav.Link as={Link} to="/contact" className={pathname === "/contact" ? selectedPageClass : notSelected}>
+                            <Nav.Link as={Link} to="/contact" onClick={closeMenu} className={pathname === "/contact" ? selectedPageClass : notSelected}>
                                 Contact
                             </Nav.Link>
                         </Nav>
